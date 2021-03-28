bdb905448209de687a9984fea68d6040fed8db9d
David PL <david.2289@icloud.com>
2021-03-28 14:39:52 +0200

    Navigation controller customised

--- iOSNotes/Controller/MainTabBarController.swift
+++ iOSNotes/Controller/MainTabBarController.swift
@@ -8,40 +8,47 @@ class MainTabBarController: UITabBarController {
 
         let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
         let homeVC = homeStoryboard.instantiateViewController(identifier: "HomeVCId")
-        // Home Nav Bar
-        let homeNC = UINavigationController(rootViewController: homeVC)
-        homeVC.navigationItem.title = NSLocalizedString("MainHomeNavBarTitle", comment: "")
-        // Home Tab Bar
-        let homeTabTitle = NSLocalizedString("MainHomeTabTitle", comment: "")
-        let homeTabIcon = UIImage(named: "IcHome")
-        let homeTabBarItem = UITabBarItem(title: homeTabTitle, image: homeTabIcon, tag: 0)
-        homeVC.tabBarItem = homeTabBarItem
+        let homeNC = getNavController(navTitleKey: "MainHomeNavBarTitle",
+                                      tabTitleKey: "MainHomeTabTitle",
+                                      iconName: "IcHome",
+                                      rootVC: homeVC)
         
         
         let topicsStoryboard = UIStoryboard(name: "Topics", bundle: nil)
         let topicsVC = topicsStoryboard.instantiateViewController(identifier: "TopicsVCId")
-        // Topics Nav Bar
-        let topicsNC = UINavigationController(rootViewController: topicsVC)
-        topicsVC.navigationItem.title = NSLocalizedString("MainTopicsNavBarTitle", comment: "")
-        // Topics Tab Bar
-        let topicsTabTitle = NSLocalizedString("MainTopicsTabTitle", comment: "")
-        let topicsTabIcon = UIImage(named: "IcList")
-        let topicsTabBarItem = UITabBarItem(title: topicsTabTitle, image: topicsTabIcon, tag: 0)
-        topicsVC.tabBarItem = topicsTabBarItem
+        let topicsNC = getNavController(navTitleKey: "MainTopicsNavBarTitle",
+                                        tabTitleKey: "MainTopicsTabTitle",
+                                        iconName: "IcList",
+                                        rootVC: topicsVC)
         
         
         let setStoryboard = UIStoryboard(name: "Settings", bundle: nil)
         let setVC = setStoryboard.instantiateViewController(identifier: "SetVCId")
-        // Settings Nav Bar
-        let setNC = UINavigationController(rootViewController: setVC)
-        setVC.navigationItem.title = NSLocalizedString("MainSettingsNavBarTitle", comment: "")
-        // Settings Tab Bar
-        let tabTitle = NSLocalizedString("MainSettingsTabTitle", comment: "")
-        let tabIcon = UIImage(named: "IcSettings")
-        let tabBarItem = UITabBarItem(title: tabTitle, image: tabIcon, tag: 0)
-        setNC.tabBarItem = tabBarItem
+        let setNC = getNavController(navTitleKey: "MainSettingsNavBarTitle",
+                                     tabTitleKey: "MainSettingsTabTitle",
+                                     iconName: "IcSettings",
+                                     rootVC: setVC)
         
+        // Selected Tab Color
+        UITabBar.appearance().tintColor = #colorLiteral(red: 0.07843137255, green: 0.6941176471, blue: 0.6705882353, alpha: 1)
         viewControllers = [homeNC, topicsNC, setNC]
+    }
+    
+    
+    private func getNavController(navTitleKey: String, tabTitleKey: String, iconName:
+                                    String, rootVC: UIViewController) -> UINavigationController {
+        // Nav Bar
+        let navController = UINavigationController(rootViewController: rootVC)
+        navController.navigationBar.barTintColor = #colorLiteral(red: 0.07843137255, green: 0.6941176471, blue: 0.6705882353, alpha: 1)
+        navController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        rootVC.navigationItem.title = NSLocalizedString(navTitleKey, comment: "")
+        // Tab Bar
+        let tabTitle = NSLocalizedString(tabTitleKey, comment: "")
+        let tabIcon = UIImage(named: iconName)
+        let tabBarItem = UITabBarItem(title: tabTitle, image: tabIcon, tag: 0)
+        rootVC.tabBarItem = tabBarItem
+        
+        return navController
     }
 
 }
